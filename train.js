76318dcde73c8b8ee30699863e59b091c6b922d5
Ozod26 <ozodfarid46@gmail.com>
2024-08-22 17:02:22 +0900

    train task bajarildi

--- train.js
+++ train.js
@@ -1,16 +1,32 @@
+/** B-TASK:
+Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+ */
+
+function countDigits(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i]) && str[i] !== " ") {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countDigits("ad2a54y79wet0sfgb92")); // 8 ta son  bor
+
 // Node.js SINGLE THREAD
 
 // array listni indexidan javobni olyapdi
 
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq xato qiling", // 20-30
-  "ozingizga ishlashni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga savmoya kiriting", // 50-60
-  "endi dam oling,  foydasi yoq endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq xato qiling", // 20-30
+//   "ozingizga ishlashni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga savmoya kiriting", // 50-60
+//   "endi dam oling,  foydasi yoq endi", // 60
+// ];
 
 // callback functionida qilamiz va uning  2 ta parametri boladi => a & callback
 // a diganda yosh narzarda tutiladi va biz yoshni past qilishiimiz kerak.
@@ -22,33 +38,33 @@ const list = [
 
 // CALLBACK function
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("Insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  // else {
-  //   setInterval(function () {
-  //     callback(null, list[5]);
-  //   }, 1000);
-  // }
-}
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+// else {
+//   setInterval(function () {
+//     callback(null, list[5]);
+//   }, 1000);
+// }
+// }
 
 // call qismi
 
 // callback functionni chaqirishda parametr sifatida function ishga tushadi va parametr ichida ish koramiz
 // else: agar xotilik bolsa null chiqishi shart emas deb elseni ichida log qildik.
 
-console.log("passed here 0");
-maslahatBering(70, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log("javob:", data);
-  }
-});
-console.log("passed here 2");
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log("javob:", data);
+//   }
+// });
+// console.log("passed here 2");
 
 // console.log("Jack Ma maslahatlari");
 // const list = [
@@ -114,9 +130,9 @@ Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkin
 MASALAN countLetter("e", "engineer") 3ni return qiladi.
  */
 
-function countLetter(a, word) {
-  return word.split(a).length - 1;
-}
-console.log(countLetter("e", "engineer"));
+// function countLetter(a, word) {
+//   return word.split(a).length - 1;
+// }
+// console.log(countLetter("e", "engineer"));
 // console.log(countLetter("n", "engineer"));
 // console.log(countLetter("g", "engineer"));
